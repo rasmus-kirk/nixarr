1111564779b840aec20a45fb417e48e05c4fe5a1
rasmus-kirk <mail@rasmuskirk.com>
2024-03-14 19:58:36 +0100

    updated site

--- docs/pandoc/script.js
+++ docs/pandoc/script.js
@@ -4,15 +4,16 @@ addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('nav a').forEach((el) => {
     const url = new URL(el.href);
     const fullurl = url.origin + url.pathname.replace(/\/$/, "");
-    console.log("fullurl", fullurl);
-    console.log("fullpath", fullpath);
-    console.log("locationpathname", location.pathname);
-    console.log("urlpathname", url.pathname);
     const onHome = fullpath === location.origin
     const urlIsHome = fullurl === location.origin
-    console.log("onhome", onHome);
-    console.log("urlIsHome", urlIsHome);
-    console.log("")
+
+    //console.log("fullurl", fullurl);
+    //console.log("fullpath", fullpath);
+    //console.log("locationpathname", location.pathname);
+    //console.log("urlpathname", url.pathname);
+    //console.log("onhome", onHome);
+    //console.log("urlIsHome", urlIsHome);
+    //console.log("")
 
     if (onHome && fullurl === fullpath) {
       el.classList.add('active');
