223b328a31f60137540bd55a65aef1c6b0a8c132
Edward Pierzchalski <e.a.pierzchalski@gmail.com>
2025-11-13 11:35:19 +1100

    [lib]: fixup: not all *arr APIs use the same version number

--- nixarr/lib/utils.nix
+++ nixarr/lib/utils.nix
@@ -7,7 +7,7 @@
     server = config.services.${service}.settings.server;
   in "http://127.0.0.1:${toString server.port}${server.urlBase}";
 
-  mkArrApiScript = service:
+  mkArrApiScript = service: version:
     pkgs.writeShellApplication {
       name = "call-${service}-api";
       runtimeInputs = [pkgs.curl];
@@ -27,17 +27,17 @@
           --insecure \
           --silent \
           --header "X-Api-Key: $apiKey" \
-          "${mkArrLocalUrl service}/api/v1/$url"
+          "${mkArrLocalUrl service}/api/v${toString version}/$url"
       '';
     };
 in {
   inherit mkArrLocalUrl;
 
-  call-lidarr-api = mkArrApiScript "lidarr";
-  call-prowlarr-api = mkArrApiScript "prowlarr";
-  call-radarr-api = mkArrApiScript "radarr";
-  call-sonarr-api = mkArrApiScript "sonarr";
+  call-lidarr-api = mkArrApiScript "lidarr" 1;
+  call-prowlarr-api = mkArrApiScript "prowlarr" 1;
+  call-radarr-api = mkArrApiScript "radarr" 3;
+  call-sonarr-api = mkArrApiScript "sonarr" 3;
   # These are blocked on https://github.com/rasmus-kirk/nixarr/pull/98
-  # call-readarr-api = mkApiScript "readarr";
-  # call-readarr-audiobook-api = mkApiScript "readarr-audiobook
+  # call-readarr-api = mkApiScript "readarr" 1;
+  # call-readarr-audiobook-api = mkApiScript "readarr-audiobook" 1;
 }
