034135757f85c2584e3455f1fcd0df68b65cd141
Edward Pierzchalski <e.a.pierzchalski@gmail.com>
2025-11-16 17:22:09 +1100

    [lib,prowlarr] replace dodgy bash scripts with slightly-less-dodgy python scripts
    The main user-facing changes are:
    
    * Removing the `call-*-api` scripts (replaced by a Python library
      providing helpers to connect to Nixarr instances via `devopsarr`
      packages).
    
    * Switching the config options from camelCase to snake_case (to match
      the snake_case used by the `devopsarr` packages).
    
    * Adding a utility (`nixarr-show-prowlarr-schemas`) to get the schemas
      (in JSON) in the format used by `devopsarr/prowlarr-py`.

--- nixarr/lib/utils.nix
+++ nixarr/lib/utils.nix
@@ -1,43 +1,54 @@
 {
   config,
-  pkgs,
+  lib,
   ...
 }: let
+  inherit
+    (lib)
+    types
+    mkOption
+    pipe
+    split
+    filter
+    isString
+    concatMapStringsSep
+    toSentenceCase
+    ;
+
   mkArrLocalUrl = service: let
     server = config.services.${service}.settings.server;
   in "http://127.0.0.1:${toString server.port}${server.urlBase}";
 
-  mkArrApiScript = service: version:
-    pkgs.writeShellApplication {
-      name = "call-${service}-api";
-      runtimeInputs = [pkgs.curl];
-      text = ''
-        url=$1
-        shift
-        apiKey=$(<'${config.nixarr.stateDir}/api-keys/${service}.key') || {
-          echo "Failed to read API key for ${service}. Has the systemd unit"
-          echo "${service}-api-key.service completed? Are you running as root,"
-          echo "or as part of the ${service} or ${service}-api groups?"
-          exit 1
-        }
-        exec curl \
-          "''${@}" \
-          --fail \
-          --location \
-          --insecure \
-          --silent \
-          --header "X-Api-Key: $apiKey" \
-          "${mkArrLocalUrl service}/api/v${toString version}/$url"
-      '';
-    };
-in {
-  inherit mkArrLocalUrl;
+  # Turns `readarr` into `Readarr` and `readarr-audiobook` into
+  # `Readarr-Audiobook`.
+  toKebabSentenceCase = str:
+    pipe str [
+      (split "-")
+      (filter isString)
+      (concatMapStringsSep "-" toSentenceCase)
+    ];
 
-  call-lidarr-api = mkArrApiScript "lidarr" 1;
-  call-prowlarr-api = mkArrApiScript "prowlarr" 1;
-  call-radarr-api = mkArrApiScript "radarr" 3;
-  call-sonarr-api = mkArrApiScript "sonarr" 3;
-  # These are blocked on https://github.com/rasmus-kirk/nixarr/pull/98
-  # call-readarr-api = mkApiScript "readarr" 1;
-  # call-readarr-audiobook-api = mkApiScript "readarr-audiobook" 1;
+  secretFileType = types.submodule {
+    options = {
+      secret = mkOption {
+        type = types.pathWith {
+          inStore = false; # Secret files should not be in the Nix store
+          absolute = true;
+        };
+        description = ''
+          Path to a file containing a secret value. Must be readable by the
+          relevant service user or group!
+        '';
+      };
+    };
+  };
+
+  arrCfgType = with types; attrsOf (oneOf [str bool int secretFileType (listOf int) (listOf str)]);
+in {
+  inherit
+    mkArrLocalUrl
+    toKebabSentenceCase
+    secretFileType
+    arrCfgType
+    ;
 }
